99b4edc20f2a801e6c534f41a057045e12769d54
Louisonn <97410495+Louisonn@users.noreply.github.com>
2022-02-08 22:00:00 +0100

    fonctionne almost

--- Simulation suivi de traj/Simulation_suivi_de_traj.py
+++ Simulation suivi de traj/Simulation_suivi_de_traj.py
@@ -1,11 +1,13 @@
-
 import pygame
 import math
 
 background_colour = (255,255,255)
-(width, height) = (1400, 400)
-objectif = 0 #height/4
+(width, height) = (1400, 1000)
+objectif = (600, 400)
+startingPoint = (400,400)
 fps = 100
+
+
 dt = 1/fps
 
 accMax = 100
@@ -18,7 +20,7 @@ class object:
         #state
         self.x = x
         self.y = y
-        self.angle = math.pi
+        self.angle = math.pi/2
         self.vel_angle = 0
         self.speed = 0
         self.acc = 0
@@ -28,23 +30,24 @@ class object:
         pygame.draw.circle(screen, self.colour, (self.x, self.y), self.size, self.thickness)
         pygame.draw.line(screen, (125,0,125), (self.x,self.y), (self.x + 50 * math.sin(math.pi - self.angle),self.y + 50 * math.cos(math.pi - self.angle)))
         pygame.draw.line(screen, (125,0,125), (0,height/4), (width,height/4))
-        print(self.angle)
+        pygame.draw.circle(screen, (125,0,125), objectif, 4, 4)
 
-    def move(self, control):
-        '''self.acc = control[0] + control[1] + control[2]
+
+    def angleCorrection(self, control):
+        self.vel_angle = control[0] + control[1] + control[2]
+        self.angle += self.vel_angle * dt
+
+
+    def speedCorrection(self, control):
+        self.acc = control[0] + control[1] + control[2]
         self.speed += self.acc*dt
         deplacement = 1/2 * self.acc*dt*dt + self.speed*dt
-        print(self.speed)
         self.x += deplacement * math.sin(math.pi - self.angle)
-        self.y += deplacement * math.cos(math.pi - self.angle)'''
-
-        self.vel_angle = control[0] + control[1] + control[2]
-
-        self.angle += self.vel_angle * dt
+        self.y += deplacement * math.cos(math.pi - self.angle)
         
 
     def getPosition(self):
-        return self.y
+        return (self.x, self.y)
 
     def getSpeed(self):
         return self.speed
@@ -75,17 +78,47 @@ class PID:
 
 
 
+def process(systeme):
+
+    actualPosition = systeme.getPosition()
+    actualSpeed = systeme.getSpeed()
+    actualAngle = systeme.getAngle()
+        
+
+
+    erreur_angle = math.atan((objectif[1] - actualPosition[1]) /(objectif[0] - actualPosition[0] + 0.01)) - actualAngle + math.pi/2
+    
+    systeme.angleCorrection(anglePID.controle(erreur_angle, actualAngle))
+
+    realDistance = math.sqrt((actualPosition[0] - objectif[0])**2 + (actualPosition[1] - objectif[1])**2)
+
+    erreur_Distance = realDistance * math.cos(erreur_angle)
+    posPIDResult = positionPID.controle(erreur_Distance, erreur_Distance)
+
+    erreur_Speed = posPIDResult[0] + posPIDResult[1] + posPIDResult[2] - actualSpeed
+    systeme.speedCorrection(speedPID.controle(erreur_Speed, actualSpeed))
+
+
+
+    
 
 
 
 
 
+
+
+
+#initialisations
 
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Tutorial 1')
 screen.fill(background_colour)
 
-particule = object(400,200)
+
+
+
+particule = object(startingPoint[0], startingPoint[1])
 particule.display()
 pygame.display.flip()
 
@@ -93,31 +126,21 @@ clock = pygame.time.Clock()
 
 position = 0
 
-speedPID = PID(3, 0, 0.7)
+positionPID =   PID(1, 0, 0)
 
-anglePID = PID(3
-               , 0, 0)
+speedPID    =   PID(1, 0, 0)
+
+anglePID    =   PID(2, 0, 0)
 
 
 
 running = True
 while running:
 
-    '''
-    position = particule.getPosition()
-
-    erreur = objectif - position'''
-
-    speed = particule.getSpeed()
-
-    angle = particule.getAngle()
-
-    #erreur_speed = objectif - speed
-    erreur_angle = objectif - angle
+    process(particule)
 
     
-
-    particule.move(anglePID.controle(erreur_angle, angle))
+    
     screen.fill(background_colour)
     particule.display()
     pygame.display.flip()
